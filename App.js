79acc9bb61b5f448bb268abaa000cb1e078d107a
Aman Mittal <amandeepmittal@live.com>
2019-11-14 19:23:02 +0530

    Adding icons to tab bar

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { mapping, light as lightTheme } from '@eva-design/eva'
-import { ApplicationProvider } from 'react-native-ui-kitten'
+import { ApplicationProvider, IconRegistry } from 'react-native-ui-kitten'
+import { EvaIconsPack } from '@ui-kitten/eva-icons'
 
 import TabNavigator from './src/navigation/TabNavigator'
 
 const App = () => (
-  <ApplicationProvider mapping={mapping} theme={lightTheme}>
-    <TabNavigator />
-  </ApplicationProvider>
+  <Fragment>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider mapping={mapping} theme={lightTheme}>
+      <TabNavigator />
+    </ApplicationProvider>
+  </Fragment>
 )
 
 export default App
--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,17 +1,45 @@
 import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { Icon } from 'react-native-ui-kitten'
 
 import Feed from '../screens/Feed'
 import AddPost from '../screens/AddPost'
 
-const TabNavigator = createBottomTabNavigator({
-  Feed: {
-    screen: Feed
+const TabNavigator = createBottomTabNavigator(
+  {
+    Feed: {
+      screen: Feed,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <Icon
+            name='home-outline'
+            width={32}
+            height={32}
+            fill={focused ? '#111' : '#939393'}
+          />
+        )
+      }
+    },
+    AddPost: {
+      screen: AddPost,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <Icon
+            name='plus-square-outline'
+            width={32}
+            height={32}
+            fill={focused ? '#111' : '#939393'}
+          />
+        )
+      }
+    }
   },
-  AddPost: {
-    screen: AddPost
+  {
+    tabBarOptions: {
+      showLabel: false
+    }
   }
-})
+)
 
 export default createAppContainer(TabNavigator)
