b1af10132945295d3dc7b15067a0233d406603b3
Aman Mittal <amandeepmittal@live.com>
2019-11-15 12:15:41 +0530

    fetch posts

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react'
-import { Image, View, StyleSheet } from 'react-native'
+import { Image, View, StyleSheet, ActivityIndicator } from 'react-native'
 import { Text, Layout, withStyles, List } from 'react-native-ui-kitten'
+import { withFirebaseHOC } from '../utils'
 
-const DATA = [
-  {
-    id: 1,
-    postTitle: 'Planet of Nature',
-    imageURI:
-      'https://images.unsplash.com/photo-1482822683622-00effad5052e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
-  },
-  {
-    id: 2,
-    postTitle: 'Lamp post',
-    imageURI:
-      'https://images.unsplash.com/photo-1482822683622-00effad5052e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
-  }
-]
+// const DATA = [
+//   {
+//     id: 1,
+//     postTitle: 'Planet of Nature',
+//     imageURI:
+//       'https://images.unsplash.com/photo-1482822683622-00effad5052e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+//   },
+//   {
+//     id: 2,
+//     postTitle: 'Lamp post',
+//     imageURI:
+//       'https://images.unsplash.com/photo-1482822683622-00effad5052e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+//   }
+// ]
 
 class _Feed extends Component {
+  state = { DATA: null, isRefreshing: false }
+  componentDidMount() {
+    this.fetchPosts()
+  }
+
+  fetchPosts = async () => {
+    try {
+      const posts = await this.props.firebase.getPosts()
+      console.log(posts)
+      this.setState({ DATA: posts, isRefreshing: false })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  onRefresh = () => {
+    this.setState({ isRefreshing: true })
+    this.fetchPosts()
+  }
+
   render() {
     const renderItem = ({ item }) => (
       <View style={this.props.themedStyle.card}>
         <Image
-          source={{ uri: item.imageURI }}
+          source={{ uri: item.postPhoto.uri }}
           style={this.props.themedStyle.cardImage}
         />
         <View style={this.props.themedStyle.cardHeader}>
@@ -32,46 +53,59 @@ class _Feed extends Component {
         </View>
       </View>
     )
-    return (
-      <Layout style={{ flex: 1 }}>
+    if (this.state.DATA != null) {
+      return (
+        <Layout style={{ flex: 1 }}>
+          <View
+            style={{
+              marginTop: 60,
+              borderBottomWidth: StyleSheet.hairlineWidth,
+              alignItems: 'center'
+            }}>
+            <Text style={{ fontSize: 20 }}>All Posts 🔥</Text>
+          </View>
+          <List
+            style={this.props.themedStyle.container}
+            data={this.state.DATA}
+            renderItem={renderItem}
+            keyExtractor={this.state.DATA.id}
+            refreshing={this.state.isRefreshing}
+            onRefresh={() => this.onRefresh()}
+          />
+        </Layout>
+      )
+    } else {
+      return (
         <View
-          style={{
-            marginTop: 60,
-            borderBottomWidth: StyleSheet.hairlineWidth,
-            alignItems: 'center'
-          }}>
-          <Text style={{ fontSize: 20 }}>All Posts 🔥</Text>
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <ActivityIndicator size='large' />
         </View>
-        <List
-          style={this.props.themedStyle.container}
-          data={DATA}
-          renderItem={renderItem}
-          keyExtractor={DATA.id}
-        />
-      </Layout>
-    )
+      )
+    }
   }
 }
 
-export default Feed = withStyles(_Feed, theme => ({
-  container: {
-    flex: 1
-  },
-  card: {
-    backgroundColor: theme['color-basic-100'],
-    marginBottom: 25
-  },
-  cardImage: {
-    width: '100%',
-    height: 300
-  },
-  cardHeader: {
-    padding: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  cardTitle: {
-    color: theme['color-basic-1000']
-  }
-}))
+export default Feed = withFirebaseHOC(
+  withStyles(_Feed, theme => ({
+    container: {
+      flex: 1
+    },
+    card: {
+      backgroundColor: theme['color-basic-100'],
+      marginBottom: 25
+    },
+    cardImage: {
+      width: '100%',
+      height: 300
+    },
+    cardHeader: {
+      padding: 10,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between'
+    },
+    cardTitle: {
+      color: theme['color-basic-1000']
+    }
+  }))
+)
